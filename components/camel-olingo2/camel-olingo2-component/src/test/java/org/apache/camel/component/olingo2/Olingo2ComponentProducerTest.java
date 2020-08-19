2573cf20bf6b34caec8566be1f74401897452379
Christoph Deppisch <cdeppisch@redhat.com>
2020-08-19 13:24:25 +0200

    [CAMEL-15407]: Fix merge operation in camel-olingo2
    Merge operation must enable isDataBasedPropertySerialization setting in the entity provider write properties in order to not overwrite unspecified fields with null values.

--- components/camel-olingo2/camel-olingo2-component/src/test/java/org/apache/camel/component/olingo2/Olingo2ComponentProducerTest.java
+++ components/camel-olingo2/camel-olingo2-component/src/test/java/org/apache/camel/component/olingo2/Olingo2ComponentProducerTest.java
@@ -162,6 +162,37 @@ public class Olingo2ComponentProducerTest extends AbstractOlingo2TestSupport {
         LOG.info("Delete status: {}", status);
     }
 
+    @Test
+    public void testCreateMerge() throws Exception {
+        final Map<String, Object> data = getEntityData();
+        data.put(ID_PROPERTY, TEST_MERGE_MANUFACTURER_ID);
+
+        final ODataEntry manufacturer = requestBody("direct:CREATE", data);
+        assertNotNull("Created Manufacturer", manufacturer);
+        final Map<String, Object> properties = manufacturer.getProperties();
+        assertEquals("Created Manufacturer Id", TEST_MERGE_MANUFACTURER_ID, properties.get(ID_PROPERTY));
+        LOG.info("Created Manufacturer: {}", properties);
+
+        final Map<String, Object> propertiesToUpdate = new HashMap<>();
+        propertiesToUpdate.put(ID_PROPERTY, TEST_MERGE_MANUFACTURER_ID);
+        propertiesToUpdate.put("Name", "MyCarManufacturer Updated");
+
+        HttpStatusCodes status = requestBody("direct:MERGE", propertiesToUpdate);
+        assertNotNull("Merge status", status );
+        assertEquals("Merge status", HttpStatusCodes.NO_CONTENT.getStatusCode(), status.getStatusCode());
+        LOG.info("Merge status: {}", status);
+
+        final Map<String, Object> headers = new HashMap<>();
+        headers.put(Olingo2Constants.PROPERTY_PREFIX + "keyPredicate", String.format("'%s'", TEST_MERGE_MANUFACTURER_ID));
+        final ODataEntry mergedManufacturer = requestBodyAndHeaders("direct:READENTRY", null, headers);
+        assertNotNull(mergedManufacturer);
+        final Map<String, Object> mergedProperties = mergedManufacturer.getProperties();
+        assertEquals("Manufacturer Id", TEST_MERGE_MANUFACTURER_ID, mergedProperties.get(ID_PROPERTY));
+        assertEquals("Manufacturer Name", "MyCarManufacturer Updated", mergedProperties.get("Name"));
+        assertNotNull("Manufacturer Address", mergedProperties.get("Address"));
+        LOG.info("Merged Manufacturer: {}", mergedProperties);
+    }
+
     private Map<String, Object> getEntityData() {
         final Map<String, Object> data = new HashMap<>();
         data.put(ID_PROPERTY, TEST_CREATE_MANUFACTURER_ID);
